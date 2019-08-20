feaf0458ba558bc6acfad3e2ea5adb71be9821f3
DM <dizhim@uw.edu>
2019-08-20 01:18:33 -0700

    boundary

--- utils/utils.py
+++ utils/utils.py
@@ -299,14 +299,13 @@ def build_targets(pred_boxes, pred_cls, target, anchors, ignore_thres):
     gx, gy = gxy.t()
     gw, gh = gwh.t()
     gi, gj = gxy.long().t()
-    ### boundary
-    gi[gi < 0] = 0
-    gj[gj < 0] = 0
-    gi[gi > nG - 1] = nG - 1
-    gj[gj > nG - 1] = nG - 1
     # Set masks
+    gi = torch.clamp(gi, 0, noobj_mask.size()[2]-1)
+    gj = torch.clamp(gj, 0, noobj_mask.size()[3]-1)
     obj_mask[b, best_n, gj, gi] = 1
     noobj_mask[b, best_n, gj, gi] = 0
+    # obj_mask[b, best_n, gj, gi] = 1
+    # noobj_mask[b, best_n, gj, gi] = 0
 
     # Set noobj mask to zero where iou exceeds ignore threshold
     for i, anchor_ious in enumerate(ious.t()):
