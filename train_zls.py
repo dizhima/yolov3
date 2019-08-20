2a99aee2051627d80a09c6b05b63ea5935f6f105
DM <dizhim@uw.edu>
2019-08-20 01:35:11 -0700

    multi gpu cuda error

--- train_zls.py
+++ train_zls.py
@@ -71,13 +71,7 @@ if __name__ == "__main__":
     # use GPU
     use_gpu = torch.cuda.is_available()
     if torch.cuda.device_count() > 1:
-        dist.init_process_group(backend='nccl',  # 'distributed backend'
-                                init_method='tcp://127.0.0.1:9999',  # distributed training init method
-                                world_size=1,  # number of nodes for distributed training
-                                rank=0)  # distributed training node rank
-        model = torch.nn.parallel.DistributedDataParallel(model)
-        model.yolo_layers = model.module.yolo_layers  # move yolo layer indices to top level
-    # model = model.to(device)
+        model = torch.nn.DataParallel(model).to(device)
 
     # Get dataloader
     dataset = ListDataset(train_path, augment=True, multiscale=opt.multiscale_training)
