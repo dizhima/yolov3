1749d31522b7fb78dae40799a95258e4487f562d
DM <dizhim@uw.edu>
2019-08-19 23:55:17 -0700

    typo

--- mymodels.py
+++ mymodels.py
@@ -195,6 +195,7 @@ class YOLOLayer(nn.Module):
         s_emb = s_emb.view(1, self.num_classes, self.num_ft, 1, 1).permute(0,2,1,3,4).contiguous()
         s_emb = s_emb.repeat(num_samples,self.num_anchors,1,grid_size,grid_size)
         v_emb = prediction[...,5:].permute(0,1,4,2,3).contiguous().repeat(1,1,self.num_anchors,1,1)
+        print(v_emb.size(), s_emb.size())
         relation = self.RN(v_emb, s_emb)
         relation = (relation.view(num_samples,self.num_anchors,self.num_classes,grid_size,grid_size)
                     .permute(0,1,3,4,2).contiguous())
